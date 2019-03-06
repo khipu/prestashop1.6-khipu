c2c9ba7c305284fe893df9a8c09a8e8ff6d84146
chaotive <leonardomontenegro123@gmail.com>
2019-03-06 12:50:15 -0300

    Updating packager

--- package.sh
+++ package.sh
@@ -1,6 +1,19 @@
+#!/usr/bin/env bash
+
 cd ..
-rm -rf khipupayment khipupayment.zip
-cp -R prestashop-khipu khipupayment
-rm -rf khipupayment/.git khipupayment/.gitignore khipupayment/.gitmodules khipupayment/.idea khipupayment/.DS_Store khipupayment/package.sh 
+rm -rf prestashop1.6-khipu-release prestashop1.6-khipu/dist
+mkdir prestashop1.6-khipu-release prestashop1.6-khipu/dist
+cp -R prestashop1.6-khipu prestashop1.6-khipu-release
+cd prestashop1.6-khipu-release
+mv prestashop1.6-khipu khipupayment
+rm -rf \
+    khipupayment/.git \
+    khipupayment/.gitignore \
+    khipupayment/.gitmodules \
+    khipupayment/.idea \
+    khipupayment/.DS_Store \
+    khipupayment/package.sh \
+    khipupayment/composer.phar
 zip -r khipupayment.zip khipupayment
-rm -rf khipupayment
+cp khipupayment.zip ../prestashop1.6-khipu/dist
+cd ../prestashop1.6-khipu
